005353c42f5f62f3cbc4dbc40515c848fec60b17
jc612177 <alok.pandey@tothenew.com>
2022-10-09 14:59:11 +0530

    Multithreading

---
+++ Multithreading/ques5.java
@@ -0,0 +1,38 @@
+package Multithreading;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ques5 {
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public static void main(String[] args) {
+        ques5 Example = new ques5();
+        new Thread(() -> {
+            Example.lock.lock();
+            System.out.println("Lock Thread 1");
+            try {
+                Thread.sleep(5000);
+                Example.condition.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread running");
+            Example.lock.unlock();
+        }).start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Example.lock.lock();
+            System.out.println("Lock Thread 2");
+            Example.condition.signal();
+            System.out.println("Thread finished working");
+            Example.lock.unlock();
+        }).start();
+    }
+}
